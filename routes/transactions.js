5c8585993b7fd19b64b3a7840b5fee039958f7a1
NaryDiniz <senainaryara@gmail.com>
2024-08-19 21:48:36 -0300

    Iniciando o front-end

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,3 +1,15 @@
 const express = require ('express')
 const router = express.Router()
-const trasactionsCotroller = require('../controllers/trantrasactionsCotroller')
+const transactionsController = require('../controllers/transactionsController');
+
+router.get('/',transactionsController.getAlltransactions);
+
+router.post('/',transactionsController.addTransaction);
+
+router.put('/:id', transactionsController.updateTrasactionPut);
+
+router.patch('/:id', transactionsController.updateTrasactionPatch);
+
+router.delete('/:id', transactionsController.deleteTransactions);
+
+module.exports = router;
