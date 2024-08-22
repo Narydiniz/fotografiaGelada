c259891a790d297083179135c5b0d3c5ddc30538
Narydiniz <senainaryara@gmail.com>
2024-08-22 10:38:47 -0300

    Modal login

---
+++ public/script.js
@@ -0,0 +1,13 @@
+
+function abrirModal(){
+  const modal =   document.getElementById('modal-login')
+  modal.classList.add('abrir')
+
+  modal.addEventListener('click', (e) => {
+    if(e.target.id == 'close' || e.target.id == 'modal-login'){
+        modal.classList.remove('abrir')
+    }
+  })
+}
+
+
