6528a2484cc4900bcbaf1fcd842df47c289575f7
NaryDiniz <senainaryara@gmail.com>
2024-08-26 20:11:47 -0300

    Modal imagem

--- public/script.js
+++ public/script.js
@@ -10,4 +10,37 @@ function abrirModal(){
   })
 }
 
+function cancelarModal(){
+  const modal =   document.getElementById('btn-modal')
+  modal.classList.add('abrir')
+
+  modal.addEventListener('click', (e) => {
+    if(e.target.id == 'btn-modal'){
+        modal.classList.remove('abrir')
+    }
+  })
+}
+
+function entarModal(){
+  const modal =   document.getElementById('btn-modal')
+  modal.classList.add('abrir')
+
+  modal.addEventListener('click', (e) => {
+    if(e.target.id == 'btn-modal'){
+        modal.classList.remove('abrir')
+    }
+  })
+}
+
+
+
+/*function openWindow(){
+  windowInformation.classList.add('show')
+  body.classList.add('show')
+};
+function closeWindow(){
+  windowInformation.classList.remove('show')
+  body.classList.remove('show')
+}*/
+
 
